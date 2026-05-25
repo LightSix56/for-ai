95d0d0893e3aa63aee3b6587edce8c272fcb5255
LightSix56 <105928444+LightSix56@users.noreply.github.com>
2026-05-25 10:54:38 +0300

    Delete game_screen_region_translator directory

--- game_screen_region_translator/app/translate/base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from abc import ABC, abstractmethod
-
-
-class TranslatorProvider(ABC):
-    @abstractmethod
-    def translate(self, text: str, source_lang: str = "EN", target_lang: str = "RU", **kwargs) -> str:
-        raise NotImplementedError
